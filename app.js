0307376f453b2930f8c4ea5e3e1b704bdd08e8b8
aneebamoezz <anibakaleem@gmail.com>
2024-08-06 13:24:21 +0500

    commit initial files

---
+++ app.js
@@ -0,0 +1,19 @@
+// const hamburger = document.querySelector("#hamburger");
+// const menu = document.querySelector("#menu");
+// const moon = document.querySelector("#moon");
+// const body = document.querySelector("body");
+// const hLinks = document.querySelectorAll("#hLink")
+
+// hamburger.addEventListener("click", () => {
+//     menu.classList.toggle("hidden")
+// })
+
+// hLinks.forEach(link => {
+//     link.addEventListener("click", () => {
+//         menu.classList.toggle("hidden")
+//     })
+// })
+
+// moon.addEventListener("click", () => {
+//     body.classList.toggle("dark")
+// })
